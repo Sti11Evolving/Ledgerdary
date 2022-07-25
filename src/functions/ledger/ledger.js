e0605e67516480a52690ee85c5481966dcbd2736
Sti11Evolving <53064072+Sti11Evolving@users.noreply.github.com>
2022-07-25 02:36:04 -0500

    development for 7/25/2022

--- src/functions/ledger/ledger.js
+++ src/functions/ledger/ledger.js
@@ -28,12 +28,25 @@ module.exports = {
       [time] INT NOT NULL,
       [reason] VARCHAR(50)
     );
+
     CREATE TABLE users
     (
       [user_id] INT PRIMARY KEY,
-      [reason] VARCHAR(50)
-    );`
-    db.run()
+      [name] VARCHAR(37)
+    );
+
+    CREATE TABLE currencies
+    (
+      [currency_id] INT IDENTITY(1,1) PRIMARY KEY,
+      [name] VARCHAR(37),
+      [symbol] VARCHAR(15),
+      [is_prefix] BOOLEAN NOT NULL CHECK (is_prefix IN (0, 1)),
+      [has_space] BOOLEAN NOT NULL CHECK (has_space IN (0, 1)),
+      [has_pulral] BOOLEAN NOT NULL CHECK (has_space IN (0, 1))
+    );`;
+
+    db.exec(createTablesQuery, errorHandler());
+    db.close();
   },
 
   async deleteLedger(guild){
